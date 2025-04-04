ae01732266389dc31d6fa7b94a37ef07ace103fc
Florian GAGNAIRE <gagnaire.flo@gmail.com>
2025-04-04 17:39:47 +0200

    try catch all fonctions

--- FrontEnd/src/js/api.js
+++ FrontEnd/src/js/api.js
@@ -1,47 +1,76 @@
 const API_URL = "http://localhost:5678/api/";
 
 export const login = async (email, password) => {
-  const response = await fetch(`${API_URL}users/login`, {
-    method: "POST",
-    body: JSON.stringify({ email, password }),
-  });
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(`${API_URL}users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+      body: JSON.stringify({ email: email, password: password }),
+    });
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      return { error: "Email ou mot de passe incorrect" };
+    }
+  } catch {
+    return { error: "Email ou mot de passe incorrect" };
+  }
 };
 
 export const getWorks = async () => {
-  const response = await fetch(`${API_URL}works`);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(`${API_URL}works`);
+    const data = await response.json();
+    return data;
+  } catch {
+    return { error: "Erreur lors de la récupération des travaux" };
+  }
 };
 
 export const getCategories = async () => {
-  const response = await fetch(`${API_URL}categories`);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(`${API_URL}categories`);
+    const data = await response.json();
+    return data;
+  } catch {
+    return { error: "Erreur lors de la récupération des catégories" };
+  }
 };
 
 export const postWork = async (work) => {
-  const response = await fetch(`${API_URL}works`, {
-    method: "POST",
-    body: JSON.stringify(work),
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(`${API_URL}works`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(work),
+    });
+    const data = await response.json();
+    return data;
+  } catch {
+    return { error: "Erreur lors de l'ajout du travail" };
+  }
 };
 
 export const deleteWork = async (id) => {
-  const response = await fetch(`${API_URL}works/${id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(`${API_URL}works/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch {
+    return { error: "Erreur lors de la suppression du travail" };
+  }
 };
